61b577a764abc327a7dab1bf0cc505a46e0af095
hestia52 <1076232032@qq.com>
2021-03-13 19:06:05 +0800

    优化

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysDemo.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysDemo.java
@@ -1,9 +1,11 @@
 package com.ruoyi.system.domain;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.ruoyi.common.annotation.Excel;
 import com.ruoyi.common.core.domain.BaseEntity;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
  * 用例对象 sys_demo
@@ -11,6 +13,10 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author ruoyi
  * @date 2021-03-11
  */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+@TableName("sysdemo")
 public class SysDemo extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
@@ -25,44 +31,4 @@ public class SysDemo extends BaseEntity
     /** 删除标志（0代表存在 2代表删除） */
     private String delFlag;
 
-    public void setDmId(Long dmId) 
-    {
-        this.dmId = dmId;
-    }
-
-    public Long getDmId() 
-    {
-        return dmId;
-    }
-    public void setDmName(String dmName) 
-    {
-        this.dmName = dmName;
-    }
-
-    public String getDmName() 
-    {
-        return dmName;
-    }
-    public void setDelFlag(String delFlag) 
-    {
-        this.delFlag = delFlag;
-    }
-
-    public String getDelFlag() 
-    {
-        return delFlag;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
-            .append("dmId", getDmId())
-            .append("dmName", getDmName())
-            .append("delFlag", getDelFlag())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .toString();
-    }
 }
